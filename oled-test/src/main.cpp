723cf6aa677792f42c6301176f0d373e4c39a164
cruddierosprey <99515038+cruddierosprey@users.noreply.github.com>
2026-09-02 19:19:27 +0300

    Use cmoz OLED_ESP32 main.cpp unchanged on ESP32-S3 target

--- oled-test/src/main.cpp
+++ oled-test/src/main.cpp
@@ -3,27 +3,64 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SDA_PIN 9
-#define SCL_PIN 8
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
+#define OLED_RESET    -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+float wavePhase = 0;
+int flashCounter = 0;
+bool flashActive = false;
 
 void setup() {
-  Wire.begin(SDA_PIN, SCL_PIN);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    while (true) delay(1000);
-  }
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
-  display.setTextColor(SSD1306_WHITE);
-  display.setTextSize(2);
-  display.setCursor(10, 12);
-  display.println("OLED OK!");
-  display.setTextSize(1);
-  display.setCursor(8, 42);
-  display.println("SDA9 / SCL8");
   display.display();
 }
 
-void loop() {}
+void drawStarburst(int centerX, int centerY, int radius, int lines) {
+  for (int i = 0; i < lines; i++) {
+    float angle = i * (2 * PI / lines);
+    int x = centerX + radius * cos(angle);
+    int y = centerY + radius * sin(angle);
+    display.drawLine(centerX, centerY, x, y, WHITE);
+  }
+}
+
+void drawWave(float phase) {
+  for (int x = 0; x < SCREEN_WIDTH; x++) {
+    int y = SCREEN_HEIGHT / 2 + 10 * sin((x + phase) * 0.1);
+    display.drawPixel(x, y, WHITE);
+  }
+}
+
+void drawFlashTransition() {
+  display.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+  display.display();
+  delay(100);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(10, 25);
+  display.println("Tinker\nTailor");
+  display.display();
+  delay(1000);
+}
+
+void loop() {
+  display.clearDisplay();
+
+  int radius = 10 + abs(sin(millis() * 0.002)) * 20;
+  drawStarburst(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, radius, 12);
+
+  drawWave(wavePhase);
+  wavePhase += 2;
+
+  display.display();
+  delay(50);
+
+  if (millis() > flashCounter + 10000) {
+    drawFlashTransition();
+    flashCounter = millis();
+  }
+}
